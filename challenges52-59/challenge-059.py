30d27afab26125e1848ef444b5af3270c2511fd8
abd.shallal <abd.shallal@gmail.com>
2019-07-18 12:47:42 +0300

    3rd day of python challenges 51-59

--- challenges52-59/challenge-059.py
+++ challenges52-59/challenge-059.py
@@ -4,7 +4,7 @@ color = ['red', 'blue', 'green', 'orange', 'black']
 select_color = random.choice(color)
 ask = 0
 while select_color != ask:
-    ask = str(input('Choose colour to guess computer colour! : '))
+    ask = str(input('Choose colour to guess computer colour! : ')).lower()
     if select_color != ask:
         print('You are probably feeling {0} right now'.format(select_color))
 print('Well done')
